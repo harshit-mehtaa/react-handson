fa59af222cc5833c9163adad74f60daad5ee16ca
Harshit Mehta <hdm23061993@gmail.com>
2020-10-17 01:13:37 +0530

    Highlight corresponding row and column for selected cell

--- src/components/sudoku/SudokuSquare.js
+++ src/components/sudoku/SudokuSquare.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function SudokuSquare(props) {
-    return (
-        <button className={props.className} onClick={props.onClick} disabled={!props.disabled}>
-            {props.value}
-        </button>
-    )
+  return (
+    <div className={props.className} onClick={props.onClick}>
+      {props.value}
+    </div>
+  );
 }
 
-export default SudokuSquare
+export default SudokuSquare;
